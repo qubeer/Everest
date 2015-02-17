fec0a2608dd0d5db0b55be3ddfa3b4bb73eec876
qubeer <qubeerlimbu@gmail.com>
2015-02-17 18:20:15 +0545

    Complete all pages

--- js/scripts.js
+++ js/scripts.js
@@ -38,33 +38,41 @@ responsive:{
 
 <!-- --------------------/ Filter Portfolio -->
 $(function () {
-		var filterList = {
-			init: function () {
-				// MixItUp plugin
-				// http://mixitup.io
-				$('#portfoliolist').mixitup({
-					targetSelector: '.portfolio',
-					filterSelector: '.filter',
-					effects: ['fade'],
-					easing: 'snap',
-					// call the hover effect
-					onMixEnd: filterList.hoverEffect()
-				});				
-			},
-			hoverEffect: function () {
-				// Simple parallax effect
-				$('#portfoliolist .portfolio').hover(
-					function () {
-						$(this).find('.label').stop().animate({bottom: 0}, 200, 'easeOutQuad');
-						$(this).find('img').stop().animate({top: -0}, 500, 'easeOutQuad');				
-					},
-					function () {
-						$(this).find('.label').stop().animate({bottom: -40}, 200, 'easeInQuad');
-						$(this).find('img').stop().animate({top: 0}, 300, 'easeOutQuad');								
-					}		
-				);}
-		};
-		// Run the show!
-		filterList.init();		
-	});	
+	var filterList = {
+		init: function () {
+			// MixItUp plugin
+			// http://mixitup.io
+			$('#portfoliolist').mixitup({
+				targetSelector: '.portfolio',
+				filterSelector: '.filter',
+				effects: ['fade'],
+				easing: 'snap'
+				// call the hover effect
+				//onMixEnd: filterList.hoverEffect()
+			});				
+		},
+		
+	};
+	// Run the show!
+	filterList.init();		
+});	
+<!-- --------------------/ ViewPort Checker -->
+$(document).ready(function(){
+	$('.skillbar').viewportChecker({
+		callbackFunction: progress			
+	});
 	
+	<!-- --------------------/ Bar Animation -->	
+	function progress(){ 
+		jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+		width:jQuery(this).attr('data-percent')
+		},5000);
+		});
+	}
+	
+});	
+	
+
+
+
